58f4bfb4c3e15bae4a501cb32643e1e9cbb59e9e
zizu <jimoheniola76@gmail.com>
2024-01-29 13:24:28 +0100

    Complete signin page functionality

--- api/contollers/auth.controller.js
+++ api/contollers/auth.controller.js
@@ -20,8 +20,7 @@ export const signup = async (req, res, next) => {
     })
     try {
         await newUser.save()
-        res.status(201).json('User created successfully')
-
+        res.status(201).json('User created successfully') 
     } catch (error) {
         next(error)
     }
@@ -36,9 +35,7 @@ export const signin = async (req, res, next) => {
         if (!validUser) return next(errorHandler(404, 'User not found'))
         const validPassword = bcryptjs.compareSync(password, validUser.password)
         if (!validPassword) return next(errorHandler(401, 'Wrong credentials'))
-        const token = jwt.sign({
-            id: validUser._id
-        }, process.env.JWT_SECRET)
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
         const {password:pass, ...rest} = validUser._doc
         res
         .cookie('access_token', token, {
