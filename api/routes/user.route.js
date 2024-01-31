662b48c83f9173948efb9801bdee72c7a7460121
zizu <jimoheniola76@gmail.com>
2024-02-01 00:49:57 +0100

    Create update user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,13 +1,9 @@
 import express from 'express'
+import { updateUser } from '../contollers/user.controller.js'
+import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router()
 
-router.get('/test', (req,res) =>{
-    res.json({
-        message:'Hello world',
-    })
-}
-
-)
+router.post ('/update/:id',verifyToken, updateUser)
 
 export default router
