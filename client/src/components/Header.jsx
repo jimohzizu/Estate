23aab39b044cf042fc8af5174359f9464b53bd68
zizu <jimoheniola76@gmail.com>
2024-02-21 13:49:00 +0100

    Complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,31 @@
 import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 import {useSelector} from 'react-redux'
+import { useEffect, useState } from 'react'
 
 
 const Header = () => {
 
   const {currentUser} = useSelector ((state) => state.user)
+  const [searchTerm, setsearchTerm] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit =(e) =>{
+   e.preventDefault()
+   const urlParams = new URLSearchParams(window.location.search)
+   urlParams.set('searchTerm', searchTerm)
+   const searchQuery = urlParams.toString()
+   navigate(`/search?${searchQuery}`)
+  }
+
+  useEffect(() =>{
+   const urlParams = new URLSearchParams(window.location.search)
+    const searchTermFromUrl = urlParams.get('searchTerm')
+
+    if(searchTermFromUrl) {
+      setsearchTerm(searchTermFromUrl)
+    }
+  },[location.search])
 
   return (
  <header className='bg-blue-200 shadow-md'>
@@ -16,9 +36,9 @@ const Header = () => {
     <span className='text-slate-700'>Estate</span>
     </h1>
     </Link>
-    <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-      <input type="text" className='bg-transparent focus:outline-none w-24 sm:w-64' placeholder='Search......' />
-      <FaSearch className='text-slate-600' />
+    <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+      <input  value={searchTerm} onChange={(e) => setsearchTerm(e.target.value)} type="text" className='bg-transparent focus:outline-none w-24 sm:w-64' placeholder='Search......' />
+      <button><FaSearch className='text-slate-600' /> </button>
     </form>
       <ul className='flex gap-4'>
          <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline hover:text-blue-600'>Home</li></Link>
