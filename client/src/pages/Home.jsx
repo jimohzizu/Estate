a1c685238b466f44025cdbbea6678a0c790bce9d
zizu <jimoheniola76@gmail.com>
2024-02-22 17:00:04 +0100

    add pre installation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -59,7 +59,7 @@ const Home = () => {
           <br /> place with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Zizu Estate is the best place to find your next perfect place to live
+          Z Estate is the best place to find your next perfect place to live
           <br />
           We have a wide range of properties for you to choose from.
         </div>
